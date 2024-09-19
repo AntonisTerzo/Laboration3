6bb0f5b6115342800079a98f828f2a8b4634aa34
AntonisTerzo <antonis.terzopoulos@gmail.com>
2024-09-20 01:08:26 +0200

    added methods for adding products and getting all the products

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -2,16 +2,19 @@ package entities;
 
 import java.time.LocalDate;
 
-public record Product(String id, String name, Category category, int rating, LocalDate date, LocalDate modifiedDate) {
+public record Product(String id, String name, Category category, int rating, LocalDate createdDate, LocalDate modifiedDate) {
      public Product {
          if (name == null || name.isBlank()) {
              throw new IllegalArgumentException("Name cannot be null or empty");
          }
-         if (date == null) {
+         if (rating < 0 || rating > 10) {
+             throw new IllegalArgumentException("Rating must be between 0 and 10");
+         }
+         if (createdDate == null) {
              throw new IllegalArgumentException("Date cannot be null");
          }
          if (modifiedDate == null) {
-             modifiedDate = date;
+             modifiedDate = createdDate;
          }
      }
 }
--- src/main/java/service/Warehouse.java
+++ src/main/java/service/Warehouse.java
@@ -1,5 +1,17 @@
 package service;
+import entities.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Warehouse {
+    private final List<Product> products = new ArrayList<>();
 
+    public void addProducts(Product product) {
+        products.add(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return List.copyOf(products);
+    }
 }
